2c2f74105a004839a57da046e94bd99cbbaf2093
Daniel <dunyto@gmail.com>
2024-11-24 13:50:31 +0100

    added test and lint

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,24 @@
-module.exports = {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov'],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
   }
 };
+
+export default config;
