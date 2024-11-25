c77b1df05e3da072aa900e2b175c73a9bd0c5d3d
jeichel09 <61138020+jeichel09@users.noreply.github.com>
2024-11-25 12:16:33 +0100

    Update final_project.test.ts

--- test/final_project.test.ts
+++ test/final_project.test.ts
@@ -1,17 +1,40 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as FinalProject from '../lib/final_project-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as FinalProject from '../lib/final_project-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/final_project-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new FinalProject.FinalProjectStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe('FinalProject Stack', () => {
+  let app: cdk.App;
+  let stack: FinalProject.FinalProjectStack;
+  let template: Template;
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  beforeEach(() => {
+    app = new cdk.App();
+    stack = new FinalProject.FinalProjectStack(app, 'MyTestStack');
+    template = Template.fromStack(stack);
+  });
+
+  test('Lambda Function Created', () => {
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      Runtime: 'nodejs18.x',
+      Handler: 'index.handler',
+    });
+  });
+
+  test('DynamoDB Table Created', () => {
+    template.hasResourceProperties('AWS::DynamoDB::Table', {
+      BillingMode: 'PAY_PER_REQUEST',
+    });
+  });
+
+  test('S3 Bucket Created', () => {
+    template.hasResourceProperties('AWS::S3::Bucket', {
+      PublicAccessBlockConfiguration: {
+        BlockPublicAcls: true,
+        BlockPublicPolicy: true,
+        IgnorePublicAcls: true,
+        RestrictPublicBuckets: true,
+      },
+    });
+  });
 });
+
